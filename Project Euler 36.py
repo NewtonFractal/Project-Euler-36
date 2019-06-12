3b84869c5dad8e98b0b7bce4e594cf4af8c546f1
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-12 08:34:49 +0200

    Update Project Euler 36.py

--- Project Euler 36.py
+++ Project Euler 36.py
@@ -2,13 +2,14 @@ import time
 start = time.time()
 
 def Double_base_palindromes(upper_bound,x):
+    Palindrome = []
     while int(x) < upper_bound:
         if str(x) == str(x[::-1]):
             if str(bin(int(x))[2:]) == str(bin(int(x))[2:])[::-1]:
-                print(x)
+                Palindrome.append(int(x))
         x = str(int(x)+1)
+    return print(sum(Palindrome))
 
 Double_base_palindromes(1000000,str(10))
-
 end = time.time()
 print(end - start)
