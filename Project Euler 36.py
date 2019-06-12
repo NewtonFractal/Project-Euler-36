ef8e74bea35378dcb921d65c263081f65cbd040f
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-12 08:50:59 +0200

    Update Project Euler 36.py

--- Project Euler 36.py
+++ Project Euler 36.py
@@ -4,7 +4,7 @@ start = time.time()
 def Double_base_palindromes(upper_bound,x):
     Palindrome = []
     while int(x) < upper_bound:
-        if str(x) == str(x[::-1]):
+        if x == x[::-1]:
             if str(bin(int(x))[2:]) == str(bin(int(x))[2:])[::-1]:
                 Palindrome.append(int(x))
         x = str(int(x)+1)
