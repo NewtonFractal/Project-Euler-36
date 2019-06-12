12fbf93f04a5bd0b36daab73f9fb9b10421bc9aa
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-12 08:21:00 +0200

    Update Project Euler 36.py

--- Project Euler 36.py
+++ Project Euler 36.py
@@ -4,7 +4,8 @@ start = time.time()
 def Double_base_palindromes(upper_bound,x):
     while int(x) < upper_bound:
         if str(x) == str(x[::-1]):
-            y = str(x)
+            if str(bin(x)[2:].zfill(8)) == str(bin(x)[2:].zfill(8)[::-1]):
+                print(x)
         x = str(int(x)+1)
 
 Double_base_palindromes(1000000,str(10))
